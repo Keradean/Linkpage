b3cd462fb5410aa4a74312b8f2ad82a36eda6143
Dennis De Col <dennis.decol@murena.io>
2026-06-23 13:02:10 +0200

    update

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+function loadVideo(videoId) {
+    const element = document.getElementById('video-container');
+
+    element.innerHTML = `
+        <iframe
+            width="100%"
+            height="240"
+            src="https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1"
+            title="YouTube Video"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
+    `;
+}
